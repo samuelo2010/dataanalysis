16ac3b78f5e30dc765a30b2ee8d9a9863af08211
Samuel Ochieng <samuelo2010@gmail.com>
2023-05-22 12:57:41 +0300

    vldata analysis rstudio source code

---
+++ rstudio_vldata_analysis_sourcecode.R
@@ -0,0 +1,16 @@
+###msc vldata analysis file
+### 1.Exploring vldata
+
+    ##navigating to git dataanalysis
+
+setwd("C:/Users/USER/Desktop/dataanalysis")
+getwd()
+
+
+###2. Data cleaning
+
+###3. Data manipulation
+
+###4. Data description and summary
+
+###5. Data analysis
