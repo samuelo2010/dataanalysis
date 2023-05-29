28c3f4f86d7c85e9fdbd0da6a126ec236c24b245
Samuel Ochieng <samuelo2010@gmail.com>
2023-05-29 15:57:15 +0300

    vldata exploration

--- rstudio_vldata_analysis_sourcecode.R
+++ rstudio_vldata_analysis_sourcecode.R
@@ -1,19 +1,60 @@
 ###msc vldata analysis file
 ### 1.Exploring vldata
 
-    ##navigating to git dataanalysis
+    ##a.navigating to git dataanalysis
 
 setwd("C:/Users/USER/Desktop/dataanalysis")
 getwd()
 
-###install tidyverse package
+###b.install tidyverse package
 install.packages("tidyverse")
 library(tidyverse)
 
-##load the vldata
+##c. load the vldata
 vldata<-read.csv("vldata_csv.csv")
 
+names(vldata)#view the existing variables
+glimpse(vldata)
+attach(vldata)#allow easy extraction of variables
+unique(County)#40 counties
+unique(District)#127 districts
+unique(Facility.Name)#1047 facilities
+unique(Sex)#Female,male,""
+unique(AgeYears)# range from 0 years but some are in year of birth
+unique(Sample.Type)# 4 sample types with no data
+unique(Collection.Date)#range from 2012 to 2015, break into year month
+unique(Received.Status)#3 status exist including rejection
+unique(Rejected.Reason...Reason.for.Repeat)#change missing into none and retain the variable
+unique(Current.Regimen)#13 regimens and retain all values
+unique(ART.Initiation.Date)#Month and Year, get duration on medication
+unique(Justification)#8 data values
+unique(Date.of.Receiving)#inform of ddmmyyyy
+unique(Date.of.Testing)#inform of ddmmyyyy
+unique(Viral.Load)#<LDL,<400,<150,Collect new sample,"" are interesting unique values
 
+
+#get frequency table for Justification
+vldata %>% 
+  select(Justification)%>%
+  count(Justification)%>%
+  arrange(desc(n))%>%
+  view()
+unique(County) #counties served
+vldata %>% 
+  select(County)%>%
+  count(County)%>%
+  arrange(desc(n))%>%
+  view()
+#explore facilities served
+unique(Facility.Name)
+vldata %>% 
+  select(Facility.Name)%>%
+  count(Facility.Name)%>%
+  arrange(desc(n))%>%
+  view()
+view(vldata[is.na(County),])# It appears the missing data isn't labelled as na
+
+summary(AgeYears)
 ###2. Data cleaning
 
 ###3. Data manipulation
